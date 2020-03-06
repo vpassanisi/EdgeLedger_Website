eb7877ae06af4bd77f2b4773a0dfa895a18c2a76
vpassanisi <vpassanisi@gmail.com>
2020-03-06 14:49:37 -0800

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+// Inititalize and add the map
+function initMap(){
+  //Your loation
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  //Center map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  //The marker, positioned at location
+  const marker = new google.maps.Marker({ postion: loc, map: map});
+}
+
+//Sticky menu background
+window.addEventListener('scroll', function(){
+  if (window.scrollY > 150){
+    this.document.querySelector('#navbar').style.opacity = 0.9;
+  } else{
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event){
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 100
+    },
+    800
+    );
+  }
+})
